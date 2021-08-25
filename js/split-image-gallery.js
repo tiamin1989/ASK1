a5014149b64582213dbef76ab284fe1b94275871
Ruslan Ramazanov <tiamin1989@yandex.ru>
2021-08-25 15:37:24 +0500

    add: timeline block && changes

---
+++ js/split-image-gallery.js
@@ -0,0 +1,22 @@
+function beforeAfter() {
+  const compares = document.querySelectorAll(".split-image-compare");
+  const inputs = document.querySelectorAll(".split-image-slider");
+  for (let i = 0; i < compares.length; i++) {
+    compares[i].style.width = inputs[i].value + "%";
+  }
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  let currentIndex = 0;
+  const bullets = document.querySelectorAll(".split-image__bullet");
+  const items = document.querySelectorAll(".split-image-item");
+  bullets.forEach((bullet, i) => {
+    bullet.addEventListener("click", () => {
+      bullets[currentIndex].classList.remove("active");
+      items[currentIndex].classList.remove("active");
+      currentIndex = i;
+      bullets[i].classList.add("active");
+      items[i].classList.add("active");
+    });
+  });
+});
