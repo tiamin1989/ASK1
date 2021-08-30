49e95192749bf9a5cf0db1bd9acfb5715bea42bf
Ruslan Ramazanov <tiamin1989@yandex.ru>
2021-08-30 10:31:13 +0500

    add: up button

---
+++ js/up.js
@@ -0,0 +1,29 @@
+let index = 0;
+
+const anchors = document.querySelectorAll(".anchor");
+const upButton = document.querySelector(".up");
+
+const config = {
+  root: null, // sets the framing element to the viewport
+  rootMargin: "150px",
+  threshold: 1,
+};
+
+const intersectionObserver = new IntersectionObserver(function (entries) {
+  if (entries[0].intersectionRatio <= 0) return;
+  index = entries[0].target.getAttribute("data-id");
+}, config);
+
+anchors.forEach((anchor) => {
+  intersectionObserver.observe(anchor);
+});
+
+upButton.addEventListener("click", () => {
+  if (index == 0) return;
+  anchors.forEach((e, i, a) => {
+    if (index == i) {
+      a[index - 1].scrollIntoView(true);
+      index--;
+    }
+  });
+});
